0e3fc8cc843a916aa8fa95207b9034f4a95c36dd
Bunyamin Genc <bunyaming@uia.no>
2023-09-13 20:32:42 +0200

    object method

---
+++ nodejs/class-notes/01-OOP_1/oop.js
@@ -0,0 +1,33 @@
+"use strict"
+
+//  Objects
+
+// const exampleObject = {
+
+// propertyName:'value',
+// methodName: function () {
+//     return ' This is method'
+// }
+
+// }
+
+// console.log(exampleObject.propertyName)
+// console.log(exampleObject.methodName)
+
+const Car = {
+
+brand: 'Ford',
+model:'Mustang',
+year: 1967,
+isAutoGear:true,
+color: ['white', 'red'],
+details: {
+    color1:'red',
+    color2: 'white',
+    engineSize:4900
+},
+
+startEngine:function (){
+    return 'Motor calisti'
+}
+}
