6b60852d80c571743621b5c4c4508d66cbf3b8c7
Bunyamin Genc <bunyaming@uia.no>
2023-09-13 20:47:34 +0200

    arrow func

--- nodejs/class-notes/01-OOP_1/oop.js
+++ nodejs/class-notes/01-OOP_1/oop.js
@@ -42,3 +42,41 @@
 // console.log(Car['details'] ['engineSize'])
 // console.log(Car['startEngine']())
 
+// **************************
+
+
+//  ! This Keyword
+
+const Car = {
+    brand: 'Ford',
+    model: 'Mustang',
+    year: 1967,
+    isAutoGear: true,
+    color: ['white', 'red'],
+    details: {
+      color1: 'red',
+      color2: 'white',
+      engineSize: 4900,
+    },
+  
+    startEngine: function () {
+      return 'Motor calisti.';
+    },
+
+    getDetails:function(){
+
+//   return this
+//    return this.brand + ' ' + this.model 
+return this.startEngine()
+
+    },
+
+   arrowFunc: () => {
+
+  return this
+
+   }
+  };
+
+// console.log(Car.getDetails ())
+console.log(Car.arrowFunc)
