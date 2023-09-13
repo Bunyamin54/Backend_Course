9bcf8895e0b46241ec13dff9ef7fc93300a961fa
Bunyamin Genc <bunyaming@uia.no>
2023-09-13 20:57:44 +0200

    Spread operators

--- nodejs/class-notes/01-OOP_1/oop.js
+++ nodejs/class-notes/01-OOP_1/oop.js
@@ -44,39 +44,56 @@
 
 // **************************
 
-
 //  ! This Keyword
 
-const Car = {
-    brand: 'Ford',
-    model: 'Mustang',
-    year: 1967,
-    isAutoGear: true,
-    color: ['white', 'red'],
-    details: {
-      color1: 'red',
-      color2: 'white',
-      engineSize: 4900,
-    },
-  
-    startEngine: function () {
-      return 'Motor calisti.';
-    },
+// const Car = {
+//     brand: 'Ford',
+//     model: 'Mustang',
+//     year: 1967,
+//     isAutoGear: true,
+//     color: ['white', 'red'],
+//     details: {
+//       color1: 'red',
+//       color2: 'white',
+//       engineSize: 4900,
+//     },
 
-    getDetails:function(){
+//     startEngine: function () {
+//       return 'Motor calisti.';
+//     },
+
+//     getDetails:function(){
 
 //   return this
-//    return this.brand + ' ' + this.model 
-return this.startEngine()
+//    return this.brand + ' ' + this.model
+// return this.startEngine()
 
-    },
+//     },
+//  objelerde claslarda arrow func kullanilmasi tavsiye edilmez cunku bos
+// Arrow functions is globalScope (Not working this keyword)
+//    arrowFunc: () => {
 
-   arrowFunc: () => {
+//   return this
 
-  return this
-
-   }
-  };
+//    }
+//   };
 
 // console.log(Car.getDetails ())
-console.log(Car.arrowFunc())
+// console.log(Car.arrowFunc())
+
+// **************************  Array Destructuring
+
+const testArray = ['value0', 'value1', 'value2', 'value3'];
+
+// const var0= testArray[0]
+// const var1= testArray[1]
+// ? siralama onemli desturcturingde
+// const [firstItem, secondItem ] =testArray
+
+// console.log(firstItem,secondItem)
+
+// ? SpreadOperator
+
+const [first, second, ...others] = testArray;
+
+console.log (first, second, others); //! 1. ve 2. veri degisken olarak digerleri spread methodu ile Array icinde geldi.
