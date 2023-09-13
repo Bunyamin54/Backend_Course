37130eddcf03d964a1fc18f72e0d0ecfbd196cab
Bunyamin Genc <bunyaming@uia.no>
2023-09-13 20:39:56 +0200

    oob

--- nodejs/class-notes/01-OOP_1/oop.js
+++ nodejs/class-notes/01-OOP_1/oop.js
@@ -14,25 +14,31 @@
 // console.log(exampleObject.propertyName)
 // console.log(exampleObject.methodName)
 
-const Car = {
-  brand: 'Ford',
-  model: 'Mustang',
-  year: 1967,
-  isAutoGear: true,
-  color: ['white', 'red'],
-  details: {
-    color1: 'red',
-    color2: 'white',
-    engineSize: 4900,
-  },
+// const Car = {
+//   brand: 'Ford',
+//   model: 'Mustang',
+//   year: 1967,
+//   isAutoGear: true,
+//   color: ['white', 'red'],
+//   details: {
+//     color1: 'red',
+//     color2: 'white',
+//     engineSize: 4900,
+//   },
 
-  startEngine: function () {
-    return 'Motor calisti.';
-  },
-};
+//   startEngine: function () {
+//     return 'Motor calisti.';
+//   },
+// };
 
-console.log(Car.brand)
-console.log(Car.color[0])
-console.log(Car.details)
-console.log(Car.details.engineSize)
-console.log(Car.startEngine())
+// console.log(Car.brand)
+// console.log(Car.color[0])
+// console.log(Car.details)
+// console.log(Car.details.engineSize)
+// console.log(Car.startEngine())
+
+// console.log(Car['brand'])
+// console.log(Car.details['engineSize'])
+// console.log(Car['details'] ['engineSize'])
+// console.log(Car['startEngine']())
+
