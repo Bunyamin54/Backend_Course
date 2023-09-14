667cbd65e73f8e7642254e7206c996e9b76ac7e4
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-09-14 22:14:03 +0200

    module.js

--- nodejs/class-notes/02-OOP_2/module.js
+++ nodejs/class-notes/02-OOP_2/module.js
@@ -2,22 +2,37 @@
 
 /* -------------------------------------------------------
     MODULES
-------------------------------------------------------- */
+------------------------------------------------------- *
 //? Başka bir JS dosyasını içe aktarma:
 
-// require('./module/index.js')
-// require('./module/index')
-// require('./module/')
+require('./module/index.js')
+require('./module/index')
+require('./module/')
 
-// // Export:
-// const test = require('./module/')
-// test()
+/* ------------------------------------------------------- *
+
+// Import:
+const test = require('./module/')
+test()
+
+/* ------------------------------------------------------- */
 
 // Shorthand for singleFunction:
 // require('./module/')()
 
 /* ------------------------------------------------------- */
 
-const {test1, test2, test3}  = require('./module/')
+// const [ test1, test2, test3 ] = require('./module/')
+// test1(), test2(), test3()
 
-test1(), test2(), test3()
+// const { test1, test2, test3 } = require('./module/')
+// test1(), test2(), test3()
+
+const { test1: newFunc, test2, test3, variable } = require('./module/')
+newFunc(), test2(), test3(), console.log(variable)
+
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
