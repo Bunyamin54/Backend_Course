44545ffd3cc16328e170eec1ee04bda6219057be
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-09-14 21:32:56 +0200

    statick methods

--- nodejs/class-notes/02-OOP_2/oop.js
+++ nodejs/class-notes/02-OOP_2/oop.js
@@ -114,7 +114,7 @@ const fordCliamate = new Accessory('Bosh Climate', 'Ford', 'Mustang', 1967, 'SUV
 // const fordCliamate = new Accessory('Bosh Climate', ...Object.values(ford)) //? Sıralama Önemli.
 console.log (fordCliamate)
 
-/* ------------------------------------------------------- */
+/* ------------------------------------------------------- *
 //? Polymorphism: Miras aldığımız sınıfın özellik/methodlarını yeniden yazabilme.
 //? Override: Üst metodla aynı isim ve yapıda yeni bir metod yazma. (ezme / iptal etme / önceliğini alma)
 //? Overload: Üst metodla aynı isimde ama farklı yapıda (parametre adet/tip) yeni method oluşturma. (aynı anda ikiside aktif) (JS desteklemez)
@@ -128,7 +128,7 @@ class Vehicle {
     }
 
     getDetails() {
-        console.log('Vehicle.getDetails')
+        console.log('Vehicle getDetails çalıştı')
         return this.vehicleType
     }
 }
@@ -153,8 +153,16 @@ class Car extends Vehicle {
 
     //? Override: Üstteki method ile aynı isimde. Artık bu geçerli.
     getDetails() {
-        console.log('Car.getDetails')
-        return this.vehicleType
+        console.log('Car getDetails çalıştı')
+        // return this
+        return {
+            brand: this.brand,
+            model: this.model,
+            year: this.year,
+            vehicleType: super.getDetails(), // Parent class metodları super ile çalıştırabilir.
+            // vehicleIsActive: super.vehicleIsActive // super-constructor bu veriyi this'e aktardı.
+            vehicleIsActive: this.vehicleIsActive // super constructor bu veriyi this'e aktardı.
+        }
     }
 }
 
@@ -162,6 +170,93 @@ const ford = new Car('Ford', 'Mustang', 1967, 'SUV')
 console.log ( ford )
 console.log ( ford.getDetails() )
 
+/* ------------------------------------------------------- *
+//? JS PUBLIC: Genel erişime açık.
+//? JS PROTECTED: Sadece Tanımlı olduğu class ve Inherit edilen child-class erişebilir.
+//? -* JS/ES12 öncesi desteklemiyor: Genel erişime açık ama lütfen dokunmayın :) 
+//? JS PRIVATE: Sadece tanımlı olduğu class içinde erişim var.
 
+
+class Vehicle {
+
+    vehicleIsActive = false // PUBLIC PROPERTY
+    _protectedProp = true // PROTECTED PROPERTY
+    #privateProp = true // PRIVATE PROPERTY
+
+    constructor(vehicleType) {
+        this.vehicleType = vehicleType
+    }
+
+    // Override yapma lütfen:
+    _protectedMethod() {
+        console.log('Vehicle protectedMethod çalıştı')
+        return true
+    }
+
+    #privateMethod() {
+        console.log('Vehicle privateMethod çalıştı')
+        return true
+    }
+
+    getDetails() {
+        console.log('Vehicle getDetails çalıştı')
+        console.log( 'privateProp', this.#privateProp )
+        console.log( this.#privateMethod() )
+    }
+}
+
+class Car extends Vehicle {
+
+    isRunning = false
+
+    constructor(brand, model, year, vehicleType = 'Car') {
+        super(vehicleType)
+        this.brand = brand
+        this.model = model
+        this.year = year
+    }
+
+    runEngine() {
+        this.isRunning = true
+        console.log('Motor Çalıştı')
+        return this.isRunning
+    }
+
+    getDetails() {
+        console.log('Car getDetails çalıştı')
+        // console.log( 'privateProp', this.#privateProp ) // NO ACCESS
+        // console.log( this.#privateMethod() ) // NO ACCESS
+        // console.log( 'privateProp', super.#privateProp ) // NO ACCESS
+        // console.log( super.#privateMethod() ) // NO ACCESS
+    }
+}
+
+const ford = new Car('Ford', 'Mustang', 1967, 'SUV')
+console.log ( ford )
+console.log ( ford.getDetails() )
+// console.log ( ford.#privateProp ) // NO ACCESS
+
+/* ------------------------------------------------------- */
+//? GETTER & SETTER METHODS
+//? Görevi veri getirme (getter) ve veri güncelleme (setter) olan metodlardır.
+
+class Car {
+
+    isRunning = false
+
+    constructor(brand, model, year) {
+        this.brand = brand
+        this.model = model
+        this.year = year
+    }
+
+    runEngine() {
+        this.isRunning = true
+        console.log('Motor Çalıştırıldı')
+        return this.isRunning
+    }
+}
+
+const ford = new Car('Ford', 'Mustang', 1967)
 
 /* ------------------------------------------------------- */
