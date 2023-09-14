aebd6be33a44c4b41c05178a2251b08fc33d1f2f
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-09-14 22:01:24 +0200

    module exports

--- nodejs/class-notes/02-OOP_2/module.js
+++ nodejs/class-notes/02-OOP_2/module.js
@@ -9,11 +9,15 @@
 // require('./module/index')
 // require('./module/')
 
-// Export:
-const test = require('./module/')
-test()
+// // Export:
+// const test = require('./module/')
+// test()
 
 // Shorthand for singleFunction:
-require('./module/')()
+// require('./module/')()
 
 /* ------------------------------------------------------- */
+
+const {test1, test2, test3}  = require('./module/')
+
+test1(), test2(), test3()
