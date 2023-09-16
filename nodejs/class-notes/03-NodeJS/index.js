6451395add4960a09cb75c379ce43d42b26a7c2b
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-09-16 13:07:46 +0200

    ENV

--- nodejs/class-notes/03-NodeJS/index.js
+++ nodejs/class-notes/03-NodeJS/index.js
@@ -8,23 +8,23 @@
 const http = require('node:http') // builtin: https://nodejs.org/api/http.html
 /* -------------------------------------------- *
 
-http.createServer((req, res) => { ... }
+// http.createServer((req, res) => { ... }
 const app = http.createServer((request, response) => {
 
     response.end('<h1> Welcome to NodeJS Server </h1>')
 
 })
 
-http://127.0.0.1:8000 
-http://localhost:8000 
+// http://127.0.0.1:8000 
+// http://localhost:8000 
 app.listen(8000, () => console.log('Server Runned: http://127.0.0.1:8000'))
 
 /* -------------------------------------------- *
 
 const app = http.createServer((req, res) => {
 
-    console.log( req )
-    console.log( res )
+    // console.log( req )
+    // console.log( res )
     console.log( req.url )
     if (req.url == '/') {
 
@@ -41,9 +41,7 @@ const app = http.createServer((req, res) => {
 
 }).listen(8000, () => console.log('Server Runned: http://127.0.0.1:8000'))
 
-/* -------------------------------------------- */
-
-
+/* -------------------------------------------- *
 
 http.createServer((req, res) => {
 
@@ -75,12 +73,12 @@ http.createServer((req, res) => {
                 'another-header': 'another-value',
             })
 
-              const obj = {
-                result : true,
-                message:'Islem basarili'
-              }
+            const obj = {
+                result: true,
+                message: 'İşlem Başarılı.'
+            }
 
-            res.end(JSON.stringify(obj) )
+            res.end( JSON.stringify(obj) )
 
         } else {
 
@@ -93,3 +91,34 @@ http.createServer((req, res) => {
     }
 
 }).listen(8000, () => console.log('http://127.0.0.1:8000'))
+
+/* -------------------------------------------- *
+// ENV
+
+// $ ENV_NAME=ENV_VALUE node index.js
+// console.log( process.env.ENV_NAME )
+
+// get ENV_VARS from .env file:
+require("dotenv").config(); // $ npm i dotenv // https://www.npmjs.com/package/dotenv
+// console.log(process.env); 
+console.log("NODE_ENV:", process.env.NODE_ENV); 
+console.log("ENV_HOST:", process.env.ENV_HOST + ':' + process.env.ENV_PORT); 
+console.log("ENV_EXAMPLE_STR:", process.env.ENV_EXAMPLE_STR); 
+console.log("ENV_EXAMPLE_TXT:", process.env.ENV_EXAMPLE_TXT.split(' ')); 
+
+/* -------------------------------------------- */
+
+
+// get ENV_VARS from .env file:
+require("dotenv").config(); // $ npm i dotenv // https://www.npmjs.com/package/dotenv
+// console.log( process.env.PORT )
+
+const PORT = process.env.ENV_PORT ?? 8000
+
+http.createServer((request, response) => {
+
+    response.end('<h1> Welcome to NodeJS Server </h1>')
+
+}).listen(PORT, () => console.log(`Server Runned: http://127.0.0.1:${PORT}`))
+
+/* -------------------------------------------- */
