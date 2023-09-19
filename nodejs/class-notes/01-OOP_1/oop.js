8c0750646ec1cbf6e77f9b9ec8994a8c6109d914
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-09-19 22:50:50 +0200

    obje and methods

--- nodejs/class-notes/01-OOP_1/oop.js
+++ nodejs/class-notes/01-OOP_1/oop.js
@@ -15,6 +15,27 @@ console.log (exampleObject.propertyName)
 console.log(exampleObject.methodName())
 
 
+ const Car =  {
+
+
+    brand:'Ford',
+    model:'Mustang',
+    year:1967,
+    isAutoGear:true,
+    color:['white', 'red'],
+    
+   details :{
+    color1:'red',
+    color2:'white',
+    engineSize:4900,
+   },
+     
+   startEngine : function () {
+    return 'Motor Calisti'
+   }
+ }
+
+
 
 
 
