c549bc49baf2c4ec1e21ccf33175898daa124312
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-09-19 23:40:36 +0200

    joson to obje parse

--- nodejs/class-notes/01-OOP_1/oop.js
+++ nodejs/class-notes/01-OOP_1/oop.js
@@ -34,7 +34,7 @@ console.log(exampleObject.methodName())
     return 'Motor Calisti'
    }
  }
-/*  ---------------------------*/
+/*  ---------------------------*
  
 const Car =  {
 
@@ -55,13 +55,14 @@ const Car =  {
    },
 
    getDetails:function() {
-    // return this
-    // return this.brand + ' ' + this.model + ' ' + this.year 
+    return this
+    return this.brand + ' ' + this.model + ' ' + this.year 
     return this.startEngine()
    },
 
    arrowFunc: () => {
    return this
+return this.brand // undefined
    
    }
   
@@ -69,229 +70,336 @@ const Car =  {
 
  console.log(Car.getDetails ())
  console.log(Car.arrowFunc())
-//  console.log(object)
 
 
-// console.log(Car.brand)
-// console.log(Car.color[0])
-// console.log(Car.details)
-// console.log(Car.details.engineSize)
-// console.log(Car.startEngine())
+ console.log(object)
 
-// console.log(Car['brand'])
-// console.log(Car.details['engineSize'])
-// console.log(Car.details['color2'])
-// console.log(Car['details'] ['enginSize'])
-// console.log(Car['startEngine']())
-//!  Objects
 
-// const exampleObject = {
+console.log(Car.brand)
+console.log(Car.color[0])
+console.log(Car.details)
+console.log(Car.details.engineSize)
+console.log(Car.startEngine())
 
-// propertyName:'value',
-// methodName: function () {
-//     return ' This is method'
-// }
+console.log(Car['brand'])
+console.log(Car.details['engineSize'])
+console.log(Car.details['color2'])
+console.log(Car['details'] ['enginSize'])
+console.log(Car['startEngine']())
+!  Objects
 
-// }
+const exampleObject = {
 
-// console.log(exampleObject.propertyName)
-// console.log(exampleObject.methodName)
+propertyName:'value',
+methodName: function () {
+    return ' This is method'
+}
 
-// const Car = {
-//   brand: 'Ford',
-//   model: 'Mustang',
-//   year: 1967,
-//   isAutoGear: true,
-//   color: ['white', 'red'],
-//   details: {
-//     color1: 'red',
-//     color2: 'white',
-//     engineSize: 4900,
-//   },
+}
 
-//   startEngine: function () {
-//     return 'Motor calisti.';
-//   },
-// };
+console.log(exampleObject.propertyName)
+console.log(exampleObject.methodName)
 
-// console.log(Car.brand)
-// console.log(Car.color[0])
-// console.log(Car.details)
-// console.log(Car.details.engineSize)
-// console.log(Car.startEngine())
+const Car = {
+  brand: 'Ford',
+  model: 'Mustang',
+  year: 1967,
+  isAutoGear: true,
+  color: ['white', 'red'],
+  details: {
+    color1: 'red',
+    color2: 'white',
+    engineSize: 4900,
+  },
 
-// console.log(Car['brand'])
-// console.log(Car.details['engineSize'])
-// console.log(Car['details'] ['engineSize'])
-// console.log(Car['startEngine']())
+  startEngine: function () {
+    return 'Motor calisti.';
+  },
+};
 
-// **************************
+console.log(Car.brand)
+console.log(Car.color[0])
+console.log(Car.details)
+console.log(Car.details.engineSize)
+console.log(Car.startEngine())
 
-//  ! This Keyword
+console.log(Car['brand'])
+console.log(Car.details['engineSize'])
+console.log(Car['details'] ['engineSize'])
+console.log(Car['startEngine']())
 
-// const Car = {
-//     brand: 'Ford',
-//     model: 'Mustang',
-//     year: 1967,
-//     isAutoGear: true,
-//     color: ['white', 'red'],
-//     details: {
-//       color1: 'red',
-//       color2: 'white',
-//       engineSize: 4900,
-//     },
+**************************
 
-//     startEngine: function () {
-//       return 'Motor calisti.';
-//     },
+ ! This Keyword
 
-//     getDetails:function(){
+const Car = {
+    brand: 'Ford',
+    model: 'Mustang',
+    year: 1967,
+    isAutoGear: true,
+    color: ['white', 'red'],
+    details: {
+      color1: 'red',
+      color2: 'white',
+      engineSize: 4900,
+    },
 
-//   return this
-//    return this.brand + ' ' + this.model
-// return this.startEngine()
+    startEngine: function () {
+      return 'Motor calisti.';
+    },
 
-//     },
-//  objelerde claslarda arrow func kullanilmasi tavsiye edilmez cunku bos
-// Arrow functions is globalScope (Not working this keyword)
-//    arrowFunc: () => {
+    getDetails:function(){
 
-//   return this
+  return this
+   return this.brand + ' ' + this.model
+return this.startEngine()
 
-//    }
-//   };
+    },
+ objelerde claslarda arrow func kullanilmasi tavsiye edilmez cunku bos
+Arrow functions is globalScope (Not working this keyword)
+   arrowFunc: () => {
 
-// console.log(Car.getDetails ())
-// console.log(Car.arrowFunc())
+  return this
 
-// ! Array Destructuring
+   }
+  };
 
-// const testArray = ['value0', 'value1', 'value2', 'value3'];
+console.log(Car.getDetails ())
+console.log(Car.arrowFunc())
 
-// const var0= testArray[0]
-// const var1= testArray[1]
-// ? siralama onemli desturcturingde
-// const [firstItem, secondItem ] =testArray
+! Array Destructuring
 
-// console.log(firstItem,secondItem)
+const testArray = ['value0', 'value1', 'value2', 'value3'];
 
-// ? RestOperator  (en sonda olmak zorunda)
+const var0= testArray[0]
+const var1= testArray[1]
+? siralama onemli desturcturingde
+const [firstItem, secondItem ] =testArray
 
-// const [first, second, ...others] = testArray;
+console.log(firstItem,secondItem)
 
-// console.log (first, second, others); //! 1. ve 2. veri degisken olarak digerleri restmethodu ile Array icinde geldi.
+? RestOperator  (en sonda olmak zorunda)
 
-// const [...arr] = testArray
+const [first, second, ...others] = testArray;
 
-// console.log(arr)
+console.log (first, second, others); //! 1. ve 2. veri degisken olarak digerleri restmethodu ile Array icinde geldi.
 
-// ? Spreadoperator (Dagitici)  diziye dizi eklemek
+const [...arr] = testArray
 
-// const newarr = [...testArray, 'new-value1', ' new-value2' ]
+console.log(arr)
 
-// console.log(newarr)
+? Spreadoperator (Dagitici)  diziye dizi eklemek
 
-// const Car = {
-//   brand: 'Ford',
-//   model: 'Mustang',
-//   year: 1967,
-//   isAutoGear: true,
-//   color: ['white', 'red'],
-//   details: {
-//     color1: 'red',
-//     color2: 'white',
-//     engineSize: 4900,
-//   },
-//   startEngine: function () {
-//     return 'Motor calisti.';
-//   },
-// };
+const newarr = [...testArray, 'new-value1', ' new-value2' ]
 
-// ! Rest
-// const {year, model, brand, ...otherItems} =Car
+console.log(newarr)
 
-// console.log(year, model, brand)
+const Car = {
+  brand: 'Ford',
+  model: 'Mustang',
+  year: 1967,
+  isAutoGear: true,
+  color: ['white', 'red'],
+  details: {
+    color1: 'red',
+    color2: 'white',
+    engineSize: 4900,
+  },
+  startEngine: function () {
+    return 'Motor calisti.';
+  },
+};
+
+! Rest
+const {year, model, brand, ...otherItems} =Car
+
+console.log(year, model, brand)
+console.log(otherItems)
+
+const {modelYear, model: newName, brand} = Car;
+
+console.log (modelYear, newName, brand);
+console.log (Car);
+
+!  Spread
+const newObj = {
+  ...Car.color,
+  newKey: 'new-value',
+};
+
+console.log (newObj);
+
+******   Object to Json   json string datadir.  json objeleri itterable degildir.
+
+const json = JSON.stringify (Car);
+console.log (typeof json, json);
+
+! JSON TO OBJECT
+
+const newObj2 = JSON.parse (json);
+
+console.log (typeof newObj2, newObj2);
+
+!object to Array  objenin valuleri array cevirebilirz
+
+const arr= [...Car]
+
+const arr =Object.entries(Car)
+console.log(arr)
+const arr2= Object.values(Car)
+console.log(arr2)
+const arr3 = Object.keys(Car)
+console.log(arr3)
+
+! ******* Constructure function
+? "NEW KEYWORD"
+
+const PascalCaseNamed = function () {
+    this.property ='value'
+}
+
+
+const CarContructor = function (brand, model, year=2011) {
+
+    this.brand= brand
+    this.model=model
+    this.isActive =true
+    this.isRunning= false
+    this.year=year
+    this.startEngine = function () {
+        this.isRunning= true
+        return ' Motor Calisti'
+    }
+}
+
+
+ const newCar = new CarContructor('Ford', 'Mustang', 1967)
+ const newCar2 = new CarContructor('Toyota', 'Corolla')
+
+ console.log(newCar)
+console.log(newCar2)
+
+console.log(newCar2.isRunning)
+console.log(newCar2.startEngine())
+console.log(newCar2.isRunning)
+
+const Car2 = {
+  brand: 'Ford',
+  model: 'Mustang',
+  year: 1967,
+  isAutoGear: true,
+  color: ['white', 'red'],
+  details: {
+    color1: 'red',
+    color2: 'white',
+    engineSize: 4900,
+  },
+  startEngine: function () {
+    return 'Motor calisti.';
+  },
+};
+
+// /* ---------------------------------------*
+const Car =  {
+
+    brand:'Ford',
+    model:'Mustang',
+    year:1967,
+    isAutoGear:true,
+    color:['white', 'red'],
+    
+   details :{
+    color1:'red',
+    color2:'white',
+    engineSize:4900,
+   },
+     
+   startEngine : function () {
+    return 'Motor Calisti'
+   },
+
+Arrray Desturcturing 
+
+const testArray = ['value0', 'value1', 'value2', 'value3']
+? Siralama onemli 
+
+const [firstItem, secondItem] = testArray
+
+console.log(firstItem, secondItem)
+
+
+! rest operator
+const [first, second, ...others] = testArray
+
+console.log(first,second,others)
+
+
+
+ 
+/* ---------------------------------------
+  
+! Spread operator  ekleme
+
+const testArray = ['value0', 'value1', 'value2', 'value3']
+
+const newArr = [...testArray, 'new-value', ' new-value2']
+
+console.log(newArr)
+
+
+
+/* ---------------------------------------*/
+
+  
+const Car =  {
+
+    brand:'Ford',
+    model:'Mustang',
+    year:1967,
+    isAutoGear:true,
+    color:['white', 'red'],
+    
+   details :{
+    color1:'red',
+    color2:'white',
+    engineSize:4900,
+   },
+     
+   startEngine : function () {
+    return 'Motor Calisti'
+   },
+
+
+}
+
+
+// const {year, model, brand, ...otherItems} = Car
+
+// console.log(year,model, brand)
 // console.log(otherItems)
 
-// const {modelYear, model: newName, brand} = Car;
+const {year:ModelYear, model:NewName, brand, ...otherItems} = Car
 
-// console.log (modelYear, newName, brand);
-// console.log (Car);
+console.log(ModelYear,NewName, brand)
 
-// !  Spread
-// const newObj = {
-//   ...Car.color,
-//   newKey: 'new-value',
-// };
+const newObj = {
 
-// console.log (newObj);
+    ...Car,
+    newKey: 'new-value'
+}
 
-// ******   Object to Json   json string datadir.  json objeleri itterable degildir.
+console.log(newObj)
+// console.log(object)
+/* ---------------------------------------*/
 
-// const json = JSON.stringify (Car);
-// console.log (typeof json, json);
+// Object to json
+const json = JSON.stringify(Car)
 
-// ! JSON TO OBJECT
+console.log(typeof json, json)
 
-// const newObj2 = JSON.parse (json);
+// !json to object
 
-// console.log (typeof newObj2, newObj2);
+const newObj2 = JSON.parse(json)
 
-// !object to Array  objenin valuleri array cevirebilirz
-
-// const arr= [...Car]
-
-// const arr =Object.entries(Car)
-// console.log(arr)
-// const arr2= Object.values(Car)
-// console.log(arr2)
-// const arr3 = Object.keys(Car)
-// console.log(arr3)
-
-// ! ******* Constructure function
-// ? "NEW KEYWORD"
-
-// const PascalCaseNamed = function () {
-//     this.property ='value'
-// }
-
-
-// const CarContructor = function (brand, model, year=2011) {
-
-//     this.brand= brand
-//     this.model=model
-//     this.isActive =true
-//     this.isRunning= false
-//     this.year=year
-//     this.startEngine = function () {
-//         this.isRunning= true
-//         return ' Motor Calisti'
-//     }
-// }
-
-
-//  const newCar = new CarContructor('Ford', 'Mustang', 1967)
-//  const newCar2 = new CarContructor('Toyota', 'Corolla')
-
-//  console.log(newCar)
-// console.log(newCar2)
-
-// console.log(newCar2.isRunning)
-// console.log(newCar2.startEngine())
-// console.log(newCar2.isRunning)
-
-// const Car2 = {
-//   brand: 'Ford',
-//   model: 'Mustang',
-//   year: 1967,
-//   isAutoGear: true,
-//   color: ['white', 'red'],
-//   details: {
-//     color1: 'red',
-//     color2: 'white',
-//     engineSize: 4900,
-//   },
-//   startEngine: function () {
-//     return 'Motor calisti.';
-//   },
-// };
+console.log(typeof newObj2, newObj2)
+/* ---------------------------------------*/
