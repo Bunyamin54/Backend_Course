83a0d516c05aa2522975898662152664b48b09bc
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-09-25 19:56:57 +0200

    PORT HOST

--- nodejs/class-notes/08-ExpressJS/app.js
+++ nodejs/class-notes/08-ExpressJS/app.js
@@ -3,3 +3,14 @@
 
 const express = require ('express')
 const app= express()
+
+// *  ENV
+
+require ('dotenv').config()
+const HOST = process.env?.HOST || 'http://localhost'
+const PORT = process.env?.PORT || 8000
+
+
+app.listen(PORT,  () => console.log(`Running on ${HOST}:${PORT}`))
+console.log(PORT)
+console.log(HOST)
