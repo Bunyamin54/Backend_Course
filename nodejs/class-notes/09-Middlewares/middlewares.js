d894e89bcc52be4db8069f9c631721caacb8a8b0
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-09-27 20:37:24 +0200

    Middlewares & USE

--- nodejs/class-notes/09-Middlewares/middlewares.js
+++ nodejs/class-notes/09-Middlewares/middlewares.js
@@ -40,29 +40,40 @@ app.get('/', (req, res) => {
     })
 })
 
-/* ------------------------------------------------------- */
+/* ------------------------------------------------------- *
 //! next() for next callBackFunction:
 
 const middleFunction1 = (req, res, next) => {
 
+    // console.log( req.query )
+    const skip = req.query.skip ?? false
+
     req.customData = 'Custom Data With Request'
     res.customDataWithResponse = 'Custom Data With Response'
 
-    next()
+    if (skip) {
+        
+        // Bir sonraki bağımsız fonksiyona git:
+        next('route')
 
+    } else {
+        // Bir sonraki callback fonksiyona git:
+        next()
+
+    }
 }
 
 const middleFunction2 = (req, res, next) => {
 
-    next()
+    // next()
     
-    // res.send({
-    //     customData: [
-    //         req.customData,
-    //         res.customDataWithResponse
-    //     ],
-    //     message: "Here is func2, first next() runned"
-    // });
+    res.send({
+        customData: [
+            req.customData,
+            res.customDataWithResponse
+        ],
+        message: "Here is func2, first next() runned"
+    });
 
 }
 
@@ -79,6 +90,46 @@ app.get('/', [ middleFunction1, middleFunction2 ], (req, res) => {
     })
 })
 
+// next('route') ile çalıştı:
+app.get('/', (req, res) => {
+    res.send({
+        message: 'next route'
+    })
+})
+
+/* ------------------------------------------------------- */
+// Middlewares & USE:
+
+const middleFunction1 = (req, res, next) => {
+
+    // console.log( req.query )
+    const skip = req.query.skip ?? false
+
+    req.customData = 'Custom Data With Request'
+    res.customDataWithResponse = 'Custom Data With Response'
+
+    if (skip) {
+        
+        // Bir sonraki bağımsız fonksiyona git:
+        next('route')
+
+    } else {
+        // Bir sonraki callback fonksiyona git:
+
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send({
+        message: 'first route'
+    })
+})
+
+app.get('/', (req, res) => {
+    res.send({
+        message: 'second route'
+    })
+})
 
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
