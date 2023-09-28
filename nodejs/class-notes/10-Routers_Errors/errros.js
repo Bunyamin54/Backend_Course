0783d681610e61eef8351fad25ce39f5f90d3efc
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-09-28 20:36:13 +0200

    try catch

--- nodejs/class-notes/10-Routers_Errors/errros.js
+++ nodejs/class-notes/10-Routers_Errors/errros.js
@@ -14,18 +14,22 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
-/* ------------------------------------------------------- *
+/* ------------------------------------------------------- */
 
 app.get('/user/:id', (req, res) => {
     const id = req.params.id ?? 0
     if (isNaN(id)) {
+        res.statusCode = 402
         throw new Error('ID is Not A Number', { cause: 'params.id='+id })
     } else {
-        res.send({ id: id })
+        res.send({ 
+            error: false, 
+            id: id
+        })
     }
 })
 
-/* ------------------------------------------------------- */
+/* ------------------------------------------------------- *
 // TRY-CATCH:
 
 app.get('/user/:id', (req, res) => {
@@ -43,12 +47,39 @@ app.get('/user/:id', (req, res) => {
     } catch (err) {
         res.send({ 
             error: true, 
-            message: err.message
+            message: err.message,
+            cause: err.cause
         })
     }
 
 })
+/* ------------------------------------------------------- */
 
+
+
+
+
+
+
+/* ------------------------------------------------------- */
+//? errorHandler kodlamanın en sonunda yer almalı.
+
+const errorHandler = (err, req, res, next) => {
+
+    const statusCode = res.statusCode ?? 500
+
+    console.log('errorHandler runned')
+
+    res.status(statusCode).send({ 
+        error: true, // special data
+        message: err.message, // Error string Message
+        cause: err.cause, // Error optional cause
+        // stack: err.stack // Error Details.
+    })
+}
+//? for run errorHandler call in use.
+//? It must be at last middleware.
+app.use(errorHandler)
 
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
