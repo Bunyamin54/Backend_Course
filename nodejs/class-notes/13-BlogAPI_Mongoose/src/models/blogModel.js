6d0602f63a4a937f9ea0cb12584a07dab1d1ad6e
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-10-04 20:45:14 +0200

    erorr message

--- nodejs/class-notes/13-BlogAPI_Mongoose/src/models/blogModel.js
+++ nodejs/class-notes/13-BlogAPI_Mongoose/src/models/blogModel.js
@@ -19,7 +19,9 @@ const nameSchema = new mongoose.Schema({
     index: false, // veri tabanlarindaki ndex kavrami aha hizli ulasmak icin bir sutuna 
     unique:false , // benzersiz kayit
     //required:true, 
-    required: [true, 'Error message on un-require']
+    required: [true, 'Error message '],
+    enum: [[0,1,2,3], 'Error-Massage'] , //   Pattern sabitleme sinirlama 
+    validate: [validateFunction, 'Error-Message' ]  // [function(data) {return true}, 'Error-Message']
 
  }
 
