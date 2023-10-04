b58f3e3331e8d9be1ae01a6ebb1e12f7e4bafa36
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-10-04 21:20:19 +0200

    controller

--- nodejs/class-notes/13-BlogAPI_Mongoose/src/controllers/blogController.js
+++ nodejs/class-notes/13-BlogAPI_Mongoose/src/controllers/blogController.js
@@ -1,4 +1,23 @@
+
+
 "use strict"
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+// https://mongoosejs.com/docs/queries.html
+
+// Catch async-errors and send to errorHandler:
+require('express-async-errors')
+
+// Call Models:
+const { BlogPost } = require('../models/blogModel')
+
+
+
+module.exports.BlogPost = {
+
+
+
+
+
+}
