44a1db032858fc7d06fcb7d269e172d0deebe084
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-10-04 20:36:50 +0200

    field name

--- nodejs/class-notes/13-BlogAPI_Mongoose/src/models/blogModel.js
+++ nodejs/class-notes/13-BlogAPI_Mongoose/src/models/blogModel.js
@@ -2,3 +2,21 @@
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+
+const mongoose = require('mongoose')
+
+const nameSchema = new mongoose.Schema({
+
+ //  _id  AUTO  CREATED 
+
+ //fieldName: String // shorthands Uaing
+
+ fieldName : {
+    type: String,   // veri tipi
+    default: null,  // dafault bir  deger atama
+    trim:true,  // gelen veriyi trimden gecirme boslugu alma trim data
+    select: true, // show /unshow 
+
+ }
+
+}, {})
