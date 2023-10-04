9cc5fb093609eb0545d423d3334d140fe7b82bed
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-10-04 19:58:02 +0200

    MONGO DB ADDED CONNETED

--- nodejs/class-notes/13-BlogAPI_Mongoose/src/dbConnection.js
+++ nodejs/class-notes/13-BlogAPI_Mongoose/src/dbConnection.js
@@ -1,4 +1,14 @@
 "use strict"
+
+const { default: mongoose } = require("mongoose")
+
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+
+
+const  mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/')
+.then (()   => console.log('DB connected *'))
+.catch ((err)   => console.log('DB NOT connected *'))
