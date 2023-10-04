9fb1eedb07de383a36ea9b40dfed694bb5d8508d
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-10-04 20:47:19 +0200

    required

--- nodejs/class-notes/13-BlogAPI_Mongoose/src/models/blogModel.js
+++ nodejs/class-notes/13-BlogAPI_Mongoose/src/models/blogModel.js
@@ -19,7 +19,7 @@ const nameSchema = new mongoose.Schema({
     index: false, // veri tabanlarindaki ndex kavrami aha hizli ulasmak icin bir sutuna 
     unique:false , // benzersiz kayit
     //required:true, 
-    required: [true, 'Error message '],
+    required: [true, 'Error message '],  // Data Json data icinde gelmesi zorunlu mu?
     enum: [[0,1,2,3], 'Error-Massage'] , //   Pattern sabitleme sinirlama 
     validate: [validateFunction, 'Error-Message' ] , // [function(data) {return true}, 'Error-Message']  veriyi filtreden geciren function
     get: function(data) {return true},  // veri cagirirken calsitrilcak functino 
