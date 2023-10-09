56de6b93ea844b616022139d1ec1a022db16e497
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-10-09 19:43:55 +0200

    module.exports

--- nodejs/class-notes/16-PersonnelAPI/src/controllers/department.controller.js
+++ nodejs/class-notes/16-PersonnelAPI/src/controllers/department.controller.js
@@ -2,3 +2,26 @@
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
+
+const Department = reqire('../models/department.model')
+
+module.exports = {
+
+    list: async (req, res) => {
+        
+        const data = await res.getModelList(Department)
+    },
+    create: async (req, res) => {
+        
+    },
+    read: async (req, res) => {
+        
+    },
+    update: async (req, res) => {
+        
+    },
+    delete: async (req, res) => {
+        
+    }
+
+}
--- nodejs/class-notes/16-PersonnelAPI/src/models/department.model.js
+++ nodejs/class-notes/16-PersonnelAPI/src/models/department.model.js
@@ -1,6 +1,20 @@
 "use strict"
+const { mongo } = require('mongoose')
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+
+const DepartmentSchema =  new mongoose.Schema({
+
+   name:{
+    type:String,
+    trim:true,
+    required: true,
+    unique: true
+   }
+},  { collection: "departmens", timestamps: true})
+
+
+module.exports = mongoose.model('Department', DepartmentSchema)
