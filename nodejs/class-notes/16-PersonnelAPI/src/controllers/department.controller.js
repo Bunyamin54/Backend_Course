ea4365e1ca8582e1ea5e60a8ca20f759ddeac7b7
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-10-09 19:58:04 +0200

    crud status

--- nodejs/class-notes/16-PersonnelAPI/src/controllers/department.controller.js
+++ nodejs/class-notes/16-PersonnelAPI/src/controllers/department.controller.js
@@ -10,17 +10,57 @@ module.exports = {
     list: async (req, res) => {
         
         const data = await res.getModelList(Department)
+
+        res.status(200).send({
+            error:false,
+            data
+        })
     },
+
+
+
     create: async (req, res) => {
-        
+
+        const data =await  Department.create(req.body)
+        res.status(201).send({
+            error:false,
+            data
+        })
     },
+
+
+
     read: async (req, res) => {
-        
+
+         const data = Department.findOne ({_id: req.params.id})
+         res.status(200).send({
+            error:false,
+            data
+        })
     },
+
+
     update: async (req, res) => {
+
+        const data =await Department.updateOne({_id: req.params.id }, req.body)
+         res.status(202).send({
+            error:false,
+            data,
+            new: await Department.findOne({ _id: req.params.id})
+        })
         
     },
+
+
     delete: async (req, res) => {
+        const data = Department.deleteOne({_id: req.params.id })
+        
+        const statusCode = 
+
+         res.status(data.deletedCount >= 1 ? 204 : 404).send({
+            error:false,
+            data
+        })
         
     }
 
