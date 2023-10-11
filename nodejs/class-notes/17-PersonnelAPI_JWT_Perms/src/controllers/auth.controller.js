5760ef9c71a4060d445a06f801723553a83ef162
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-10-11 19:51:35 +0200

    isActive

--- nodejs/class-notes/17-PersonnelAPI_JWT_Perms/src/controllers/auth.controller.js
+++ nodejs/class-notes/17-PersonnelAPI_JWT_Perms/src/controllers/auth.controller.js
@@ -3,9 +3,48 @@
 
  EXPRESS - PERSONAL API
 ------------------------------------*/
+// JWT
+// NPM I JSONWEBTOKEN
 
+ const Personnel = require ('../models/personnel.model')
 module.exports = {
 
+    login : async (req, res ) => {
     
+     const { username, password}  = req.body  
+
+     if () username && password)  {
+
+        const user = await Personnel.findOne ({username, password })
+
+          if (user)  {
+
+            if (user.isActive)  {
+
+            } else{
+
+                res.errorStatusCode = 401
+                    throw new Error('This account is not active.')
+            }
+
+
+          }
+     }
+
+    },
+
+
+    refresh : async (req, res ) => {
+
+
+    },
+
+
+    logout : async (req, res ) => {
+
+
+    }
+
+
 
 }
