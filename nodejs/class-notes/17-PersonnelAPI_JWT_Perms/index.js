0afb588fa69ea2cb84454cb8e31803f9498cac89
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-10-11 21:41:25 +0200

    midleware

--- nodejs/class-notes/17-PersonnelAPI_JWT_Perms/index.js
+++ nodejs/class-notes/17-PersonnelAPI_JWT_Perms/index.js
@@ -7,7 +7,6 @@
     $ npm i cookie-session
     $ npm i jsonwebtoken
 */
-
 const express = require('express')
 const app = express()
 
@@ -40,24 +39,46 @@ app.use(require('cookie-session')({ secret: process.env.SECRET_KEY }))
 // res.getModelList():
 app.use(require('./src/middlewares/findSearchSortPage'))
 
-// Login/Logout Control Middleware
-app.use(async (req, res, next) => {
+// Cookie: Login/Logout Control Middleware
+// app.use(async (req, res, next) => {
 
-    const Personnel = require('./src/models/personnel.model')
+//     const Personnel = require('./src/models/personnel.model')
+
+//     req.isLogin = false
+
+//     if (req.session?.id) {
+
+//         const user = await Personnel.findOne({ _id: req.session.id })
+
+//         // if (user && user.password == req.session.password) {
+//         //     req.isLogin = true
+//         // }
+//         req.isLogin = user && user.password == req.session.password
+//     }
+//     console.log('isLogin: ', req.isLogin)
+
+//     next()
+// })
+
+const jwt = require('jsonwebtoken')
+app.use((req, res, next) => {
+
+    const auth = req.headers?.authorization || null // get Authorization
+    const accessToken = auth ? auth.split(' ')[1] : null // get JWT
 
     req.isLogin = false
 
-    if (req.session?.id) {
-
-        const user = await Personnel.findOne({ _id: req.session.id })
-
-        // if (user && user.password == req.session.password) {
-        //     req.isLogin = true
-        // }
-        req.isLogin = user && user.password == req.session.password
-    }
-    console.log('isLogin: ', req.isLogin)
-
+    jwt.verify(accessToken, process.env.ACCESS_KEY, function(err, user) {
+        if (err) {
+            req.user = null
+            console.log('JWT Login: NO')
+        } else {
+            req.isLogin = true
+            req.user = user
+            // req.user = user.isActive ? user : null
+            console.log('JWT Login: YES')
+        }
+    })
     next()
 })
 
@@ -69,14 +90,15 @@ app.all('/', (req, res) => {
     res.send({
         error: false,
         message: 'Welcome to PERSONNEL API',
-        session: req.session,
-        isLogin: req.isLogin
+        // session: req.session,
+        isLogin: req.isLogin,
+        user: req.user
     })
 })
 
-// /departments
+// /auth
 app.use('/auth', require('./src/routes/auth.router'))
-
+// /departments
 app.use('/departments', require('./src/routes/department.router'))
 // /personnels
 app.use('/personnels', require('./src/routes/personnel.router'))
