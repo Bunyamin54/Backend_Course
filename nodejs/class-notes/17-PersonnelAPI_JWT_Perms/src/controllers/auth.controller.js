f29a542e386765632d4658c76cc8e2505f4b2737
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-10-11 20:38:07 +0200

    accsess token

--- nodejs/class-notes/17-PersonnelAPI_JWT_Perms/src/controllers/auth.controller.js
+++ nodejs/class-notes/17-PersonnelAPI_JWT_Perms/src/controllers/auth.controller.js
@@ -1,77 +1,78 @@
 "use strict"
-/* ---------------------------------
-
- EXPRESS - PERSONAL API
-------------------------------------*/
+/* -------------------------------------------------------
+    EXPRESS - Personnel API
+------------------------------------------------------- */
 // JWT
-// NPM I JSONWEBTOKEN
+// npm i jsonwebtoken
+
 const jwt = require('jsonwebtoken')
- const Personnel = require ('../models/personnel.model')
+const Personnel = require('../models/personnel.model')
+
 module.exports = {
 
-    login : async (req, res ) => {
-    
-     const { username, password}  = req.body  
+    login: async (req, res) => {
 
-     if () username && password)  {
+        const { username, password } = req.body
 
-        const user = await Personnel.findOne ({username, password })
-        
+        if (username && password) {
 
-          if (user)  {
+            const user = await Personnel.findOne({ username, password })
 
-            if (user.isActive)  {
+            if (user) {
+
+                if (user.isActive) {
+                // Login OK
+
+                    const accessData = {
+                        _id: user._id,
+                        departmentId: user.departmentId,
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                        isActive: user.isActive,
+                        isAdmin: user.isAdmin,
+                        isLead: user.isLead,
+                    }
+                    const accessToken = jwt.sign(accessData, process.env.ACCESS_KEY, { expiresIn: '30m' })
+
+                    const refreshData = {
+                        username: user.username,
+                        password: user.password
+                    }
+                    const refreshToken = jwt.sign(refreshData, process.env.REFRESH_KEY, { expiresIn: '3d' })
+
+                    res.send ( {
+
+                        error : false,
+                        token: {
+                            accessToken,
+                            refreshToken
+                        }
+                    })
 
 
-                const jwtData = {
-                    _id : user._id,
-                    departmentId : user.departmentId,
-                    firstName: user.firstName,
-                    lastName:user.lastName,
-                    isActive: user.isActive,
-                    isAdmin: user.isAdmin,
-                    isLead: user.isLead
+                } else {
+
+                    res.errorStatusCode = 401
+                    throw new Error('This account is not active.')
                 }
-
-
-                  const accessToken = jwt.sign(accessData,  process.env.SECRET_KEY,  {expiresIn: '30m'})
-                 
-                 
-                  const refreshData = {
-                    username: user.username,
-                   password: user.password
-                 } 
-
-                 const refreshToken = jwt.sign(refreshData, process.env.REFRESH_KEY , { expiresIn: '3d'} )
-                  
-            } else{
+            } else {
 
                 res.errorStatusCode = 401
-                    throw new Error('This account is not active.')
-            } 
-
-
-          } else {
+                throw new Error('Wrong username or password.')
+            }
+        } else {
 
             res.errorStatusCode = 401
-                    throw new Error('Wrong username or password.')
-          }
-     }
+            throw new Error('Please enter username and password.')
+        }
+    },
+
+    refresh: async (req, res) => {
 
     },
 
-
-    refresh : async (req, res ) => {
-
+    logout: async (req, res) => {
 
     },
-
-
-    logout : async (req, res ) => {
-
-
-    }
-
-
 
 }
