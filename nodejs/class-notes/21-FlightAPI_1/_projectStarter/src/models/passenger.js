8c6f7b0155834880b5dc4bac8fbfd4b92b3e7d3c
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-10-22 23:55:55 +0200

    passengers models

--- nodejs/class-notes/21-FlightAPI_1/_projectStarter/src/models/passenger.js
+++ nodejs/class-notes/21-FlightAPI_1/_projectStarter/src/models/passenger.js
@@ -2,3 +2,22 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+const { mongoose } = require('../configs/dbConnection')
+/* ------------------------------------------------------- *
+{
+    "firstName": "Test 1",
+    "lastName": "Test 1",
+    "gerder": "M",
+    "email": "test1@site.com",
+    "createdId": "652ceaa1bae9cde5e8a97522"
+}
+/* ------------------------------------------------------- */
+// Passenger Model:
+
+const PassengerSchema = new mongoose.Schema({
+
+
+}, { collection: 'passengers', timestamps: true })
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('Passenger', PassengerSchema)
