7dbc8542b4873e1d0801ba2dcfa16a714e65d999
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-10-22 23:51:05 +0200

    router

--- nodejs/class-notes/21-FlightAPI_1/_projectStarter/src/routes/flight.js
+++ nodejs/class-notes/21-FlightAPI_1/_projectStarter/src/routes/flight.js
@@ -2,3 +2,24 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+const router = require('express').Router()
+/* ------------------------------------------------------- */
+// routes/flight:
+
+const permissions = require('../middlewares/permissions')
+const flight = require('../controllers/flight')
+
+// URL: /Flights
+
+router.route('/')
+    .get(permissions.isAdmin, flight.list)
+    .post(flight.create)
+
+router.route('/:id')
+    .get(permissions.isLogin, flight.read)
+    .put(permissions.isLogin, flight.update)
+    .patch(permissions.isLogin, flight.update)
+    .delete(permissions.isAdmin, flight.delete)
+
+/* ------------------------------------------------------- */
+module.exports = router
